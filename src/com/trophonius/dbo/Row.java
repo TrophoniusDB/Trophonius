cb0154c1efd2c70c18455069ce82198fd336da92
Terje Berg-Hansen <terje@itfakultetet.no>
2020-05-16 13:52:24 +0200

    Select * from

--- src/com/trophonius/dbo/Row.java
+++ src/com/trophonius/dbo/Row.java
@@ -25,43 +25,34 @@ public class Row<E> implements Serializable {
         this.row = row;
     }
 
-
     public void addToRow (String fieldName, E value ) {
         this.row.put(fieldName, value);
     }
 
-
-
     // Append a row to en existing table file
     public void writeRowsToDisk(E primaryKey, Row row, String dbName, String tableName) {
-        // a table of rows consists of a TreeMap with the primary key as the key
-        // and the corresponding row as the value
-
+        // a row consists of a primary key followed by the corresponding row
 /*
         TreeMap<E, Row> rows = new TreeMap<>();
         rows.put(primaryKey, row);
 */
-
-
         try {
-
             // check that table file exists in data directory
             if (java.nio.file.Files.isRegularFile(Paths.get("data/" + dbName + "/" + tableName + ".tbl"))) {
 
                 // Open table file for writing, and append map of primary key + row to the file
 
-                /*Files.delete(Paths.get("data/" + dbName + "/" + tableName + ".tbl"));
-                Files.createFile(Paths.get("data/" + dbName + "/" + tableName + ".tbl"));*/
-
                 FileOutputStream dbFile = new FileOutputStream("data/" + dbName + "/" + tableName + ".tbl",true);
                 AppendableObjectOutputStream os = new AppendableObjectOutputStream(new BufferedOutputStream(dbFile));
+                // Write the primary key
                 os.writeObject(primaryKey);
+                // write the row
                 os.writeObject(row);
                 os.flush();
                 os.close();
                 dbFile.flush();
                 dbFile.close();
-                System.out.println("Success: 1 row written to table "+tableName);
+                System.out.println("Success: 1 row written to table; "+tableName);
             } else  {
                 // Table file does not exists
                 System.out.println("Table file does not exist");
@@ -69,13 +60,11 @@ public class Row<E> implements Serializable {
             }
 
         } catch (IOException e) {
-            System.out.println("Row could not we written to disk: ");
+            System.out.println("Row could not we written to file for table: "+tableName);
             e.printStackTrace();
 
         }
     }
-
-
 
     @Override
     public String toString() {
