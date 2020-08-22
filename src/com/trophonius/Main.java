66e5dc37fb618d5ed580f35cb8ea30ad3405a34e
Terje Berg-hansen <terje@itfakultetet.no>
2020-08-22 22:07:34 +0200

    Logback & slf4j

--- src/com/trophonius/Main.java
+++ src/com/trophonius/Main.java
@@ -2,6 +2,8 @@ package com.trophonius;
 
 import com.trophonius.dbo.Database;
 import com.trophonius.sql.SqlParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,10 +15,13 @@ public class Main {
     public static Database currentDB = new Database();
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Logger logger = LoggerFactory.getLogger(Main.class);
 
         System.out.println("Welcome to Trophonius - a Simple and Fast Database");
         System.out.println("Version 0.0.1");
         System.out.println("Type 'help' or '\\h' for help and 'quit' or '\\q' to exit");
+        logger.info("Trophonius started");
+
 
         // Main Menu
         while (true) {
